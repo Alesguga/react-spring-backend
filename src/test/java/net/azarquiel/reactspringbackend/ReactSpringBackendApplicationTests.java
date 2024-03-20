b8048119b50f1a52bdf2b71a988827361d624e6e
Alejandro Gutiérrez Galán <alexgutigalan04@gmail.com>
2024-03-20 18:25:53 +0100

    Initial commit

---
+++ src/test/java/net/azarquiel/reactspringbackend/ReactSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.azarquiel.reactspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
